d62e1af93d9ca7daeb6dc47bc79557bc59a79e07
AT727 <atxponh@gmail.com>
2023-09-20 20:52:48 -0400

    fixed motion profile time cal, added PID tuning, button binding

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,14 +6,23 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.arm.commands.ArmToPose;
+import frc.robot.util.Controller;
+import frc.robot.subsystems.arm.Arm;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class RobotContainer {
+
+  private final Controller controller = new Controller(0);
+  private Arm arm = new Arm();
+
   public RobotContainer() {
     configureBindings();
   }
 
   private void configureBindings() {
-    
+    //TODO make sure this is right for changing the setpoint
+    controller.onPress(controller.A, new ArmToPose(arm, SmartDashboard.getEntry("setpoint").getDouble(Constants.Arm.kMinAngle)));
   }
 
   public Command getAutonomousCommand() {
---
+++ src/main/java/frc/robot/subsystems/arm/commands/ArmToPose.java
@@ -0,0 +1,65 @@
+package frc.robot.subsystems.arm.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.arm.Arm;
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.Timer;
+
+
+
+public class ArmToPose extends CommandBase {
+    Arm arm;
+    double setpoint;
+    TrapezoidProfile trapezoidProfile;
+    TrapezoidProfile.Constraints constraints = new TrapezoidProfile.Constraints(
+        Constants.Arm.maxVel, 
+        Constants.Arm.maxAccel);
+    Timer timer = new Timer();
+    double trapezoidPofileStartTime;
+    
+
+    public ArmToPose(Arm arm, double setpoint) {
+        this.arm = arm;
+        this.setpoint = setpoint;
+        
+
+        //create new profile, input current position & velocity, set goal to stop at new position
+        this.trapezoidProfile = new TrapezoidProfile(this.constraints, new TrapezoidProfile.State(setpoint, 0), arm.getAnchorState());
+        //set timer to 0
+        trapezoidPofileStartTime = timer.getFPGATimestamp();
+
+        addRequirements(arm);
+    }
+
+    @Override
+    public void initialize(){
+
+    }
+
+    @Override
+    public void execute(){
+        double currentTime = timer.getFPGATimestamp();
+        //calculate position & velocity every loop
+        TrapezoidProfile.State setpointState = trapezoidProfile.calculate(currentTime - trapezoidPofileStartTime);
+        //calculate feedforward for goal position & velocity
+        double arbFFVoltage = Constants.Arm.ARM_FEEDFORWARD.calculate(setpointState.position, setpointState.velocity);
+        //set position & feedforward
+        arm.setPosition(setpointState.position, arbFFVoltage);
+        
+        trapezoidPofileStartTime = timer.getFPGATimestamp();
+        //trapezoidPofileStartTime = currentTime;
+    }
+
+    @Override
+    public boolean isFinished(){
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted){
+
+    }
+}
